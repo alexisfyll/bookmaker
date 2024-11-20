bca3b132b7eff85fbf1150549ef9baf495fd2032
Alexis Fayolle <alexisfayolle@yahoo.fr>
2024-11-20 15:32:51 +0100

    Function to generate new game_reports rows

--- bookmaker/functions/bg_interactions.py
+++ bookmaker/functions/bg_interactions.py
@@ -5,7 +5,6 @@ import os
 
 
 
-
 # Load environment variables from .env file
 load_dotenv()
 project_id = os.getenv('PROJECT_ID')
@@ -55,3 +54,22 @@ def fn_get_database_game_ids(competitions_ids: list, seasons: list):
     """
 
     return read_gbq(query, project_id=project_id)
+
+
+def fn_generate_game_reports(df_new_games: pd.DataFrame):
+    """
+    Generate the game_reports rows for the new games scrapped.
+
+    Parameters:
+    df_new_games (pd.DataFrame): A dataframe containing at least game_id, home_id, away_id.
+
+    Returns:
+    pd.DataFrame: A dataframe containing the game reports for the new games in the database.
+
+    """
+    df_game_reports = pd.DataFrame()
+    for i in range (df_new_games.shape[0]):
+        df_temp = fn_generate_game_report(df_new_games['id'].iloc[i], df_new_games['home_id'].iloc[i], df_new_games['away_id'].iloc[i])
+        df_game_reports = pd.concat([df_game_reports, df_temp], ignore_index=True)    
+
+    return df_game_reports
