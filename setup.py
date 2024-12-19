916e666466b50d64191c98a7076f26ab19e03295
Alexis Fayolle <alexisfayolle@yahoo.fr>
2024-12-19 16:07:57 +0100

    back fill script

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(name='bookmaker',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/bookmaker-update-data'],
+      scripts=['scripts/bookmaker-update-data', 'scripts/bookmaker-backfill-data'],
       zip_safe=False)
