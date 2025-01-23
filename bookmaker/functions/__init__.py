0b2d799ab23e07e689d2eb211ccbaa6ff155ba28
Alexis Fayolle <alexisfayolle@yahoo.fr>
2025-01-23 15:28:25 +0100

    minors updates

--- bookmaker/functions/__init__.py
+++ bookmaker/functions/__init__.py
@@ -1,3 +1,4 @@
 # This file marks the directory as a Python package
 from .fbrf_scrapping import *
 from .bg_interactions import *
+from .exceptions import *
--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,3 +1,1 @@
 # This file marks the directory as a Python package
-# from bookmaker.scripts.bookmaker_backfill_data import *
-# from bookmaker.scripts.bookmaker_update_data import *
